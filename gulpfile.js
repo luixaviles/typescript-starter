2827b2bc30f6283e097a34161218c769374c5589
luixaviles <luis.avilesz@gmail.com>
2017-10-11 20:36:00 -0400

    Minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json", { noEmitOnError: true });
+var tsProject = ts.createProject("tsconfig.json", 
+{ noEmitOnError: true });
 
 gulp.task("build", function () {
     var tsResult = tsProject.src()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-let helloWorld:string = 'Hello TypeScript worlds';
+let helloWorld:string = 'Hello TypeScript world';
 let numberContainer:number = 2;
-// numberContainer.trim();
 console.log(helloWorld);
